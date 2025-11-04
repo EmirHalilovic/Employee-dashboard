35d51f283a4ee1e25d93aba32a3022cbbb548dc9
Emir Halilovic <emir.halilovic.tz@hotmail.com>
2025-11-04 20:19:07 +0100

    Trimming waste code

--- test-api.ts
+++
@@ -1,9 +0,0 @@
-import { getTimeChanges } from "./src/api/client";
-
-async function test() {
-  const data = await getTimeChanges();
-  console.log("Fetched entries:", data.length);
-  console.log("First entry:", data[0]);
-}
-
-test();
